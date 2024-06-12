214f971c79a0c24192eaa0f213ec7f21b33676ed
Aash <31497659+AashPau@users.noreply.github.com>
2024-06-12 10:29:37 +1000

    add my resume link

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -2,36 +2,39 @@ import React from "react";
 import me from "../Image/me.jpg";
 
 export const Header = () => {
-	return (
-		<div>
-			<div className="text-center mb-5">
-				<div className="row">
-					<div className="col-md-2">
-						<img
-							className="rounded"
-							src={me}
-							alt="profile_pic"
-							width="180rem"
-						/>
-					</div>
-					<div className="col-md-10">
-						<h1>Aashish Paudel</h1>
-						<p>Software Developer</p>
-						<p>Phone: 04xxxxx | email: aashpau@gmail.com</p>
-						<a href="" className="btn btn-primary">
-							Download Resume
-						</a>
-						<br />
-						<a
-							href="https://github.com/AashPau"
-							className="btn btn-secondary mt-2"
-							target="_blank"
-						>
-							Github Profile
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="text-center mb-5">
+        <div className="row">
+          <div className="col-md-2">
+            <img
+              className="rounded"
+              src={me}
+              alt="profile_pic"
+              width="180rem"
+            />
+          </div>
+          <div className="col-md-10">
+            <h1>Aashish Paudel</h1>
+            <p>Software Developer</p>
+            <p>Phone: 04xxxxx | email: aashpau@gmail.com</p>
+            <a
+              href="https://1drv.ms/w/s!AkrM3vJXJiv3lPwh1Py4Igzf-DEjDw?e=UyJpZq"
+              className="btn btn-primary"
+            >
+              Download Resume
+            </a>
+            <br />
+            <a
+              href="https://github.com/AashPau"
+              className="btn btn-secondary mt-2"
+              target="_blank"
+            >
+              Github Profile
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
